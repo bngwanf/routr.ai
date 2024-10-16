ba6197cc856c5a816e449adf67271b9102d0b182
Brendan Enang <bnn.cdla@gmail.com>
2024-10-15 17:51:45 -0700

    Set up core project settings and configurations in RoutrProject directory

---
+++ RoutrProject/RoutrProject/urls.py
@@ -0,0 +1,31 @@
+"""
+URL configuration for TripLogger project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.contrib.auth.views import LogoutView
+from django.urls import path, include
+from RoutrApp import urls as RoutrApp_urls
+
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+    path('accounts/logout/', LogoutView.as_view(next_page='login'), name='logout'),
+
+    path('accounts/', include('django.contrib.auth.urls')),  # Add this line
+
+    path('', include(RoutrApp_urls)),
+
+]
